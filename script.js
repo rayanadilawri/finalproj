2bd151adb36912d43fcc1e297204614bcaf89ccc
rayanadilawri <dilar840@newschool.edu>
2025-04-17 17:00:28 -0400

    omg accidentally just messaed this all up.
    adding my original JS in ahhhh i almsot just ruined this all #stress
    
    adding notes to remember WHAT EVERYTHING DOESSSS

--- script.js
+++ script.js
@@ -1,70 +1,76 @@
 let globaldata;
+
 // Function to render your items
 const renderItems = () => {
 	// The `ul` where the items will be inserted
 	const output = document.querySelector('.output')
 
-    const time = document.querySelector('#time');
-    const effort = document.querySelector('#effort');
+	const time = document.querySelector('#time');
+	const effort = document.querySelector('#effort');
 
 	// Loop through each item in the data array
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-	//data.forEach((item) => {
-		// Make a “template literal” as we have before, inserting your data (and maybe the class)
-		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-		let localdata = [];
-        for (let i=0; i<globaldata.length; i++){
-        
-			if (effort.value == "" && time.value == ""){ localdata.push(globaldata[i]);}
-			else if (effort.value == ""){ localdata.push(globaldata[i]); }
-			else if (time.value == ""){ localdata.push(globaldata[i]); }
-				//add same for type//
+	// data.forEach((item) => {
+	// Make a “template literal” as we have before, inserting your data (and maybe the class)
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
-			else if (globaldata[i].effort == effort.value && globaldata[i].time == time.value){
-            localdata.push(globaldata[i]); 
-        }
-        };
+	let localdata = [];
 
-        console.log(localdata);
-        
+	for (let i = 0; i < globaldata.length; i++) {
+		let item = globaldata[i];
 
+		// Case 1: If both time and effort are empty, include everything
+		if (effort.value === "" && time.value === "") {
+			localdata.push(item);
+		}
+		// Case 2: Only time is selected — match time only
+		else if (effort.value === "" && item.time === time.value) {
+			localdata.push(item);
+		}
+		// Case 3: Only effort is selected — match effort only
+		else if (time.value === "" && item.effort === effort.value) {
+			localdata.push(item);
+		}
+		// Case 4: Both filters are selected — match both
+		else if (item.time === time.value && item.effort === effort.value) {
+			localdata.push(item);
+		}
+		// add same for type if needed
+	}
 
+	// to do list
+	// get selector in variables available to me - time and effort so I know what the user is inputting
+	// run through a for loop to go through global data
+	// if the data in the for loop has an effort that is = to what the user has selected 
+	// and at the same time the time is the same as what the user has selected, then append that data to the local data
+	// the for loop goes through every entry in the global data and checks if the effort and time is the same as the user input
+	// if it is, then it will append that data to the local data, otherwise it will pass
+	// specific pages to look at - w3 school page for loops
+	// mdn or w3 for getting an element by id
+	// look up w3 and mdn for appending data to an array
 
-//to do list
-// get selector in variables available to me - time and effort so I know what the user is inputting
-// run through a for loop to go through global data
-// if the data in the for loop has an effort that is = to what the user has selected 
-// and at the same time the time is the same as what the user has selected, then append that data to the local data
-// the for loop goes through every entry in the global data and checks if the effort and time is the same as the user input
-// if it is, then it will append that data to the local data, otherwise it will pass
-// specific pages to look at - w3 school page for loops
-// mdn or w3 for getting an element by id
-// look up w3 and mdn for appending data to an array
+	// If we found matching items, show one randomly
+	if (localdata.length > 0) {
+		let randomnumber = Math.floor(Math.random() * localdata.length);
+		let item = localdata[randomnumber];
 
-    
-
-
-        let randomnumber = Math.floor(Math.random() * localdata.length);
-        let item = localdata[randomnumber];
-        let listItem = 
-			`
+		let listItem = `
 			<li>
 				<h2>${item.activity}</h2>
 				<p><strong>Time:</strong> ${item.time}</p>
 				<p><em>Effort: ${item.effort}</em></p>
 			</li>
-			`
-		output.innerHTML=listItem;
-         // Add it to the `ul`!
-		// Don’t feel limited to `ul > li` for these—you can insert any DOM, anywhere!
-//	})
+		`;
+
+		output.innerHTML = listItem;
+	} else {
+		output.innerHTML = `<li><p>No activities found for your selection. Try changing your filters!</p></li>`;
+	}
 }
 
-let generate_button = document.querySelector('.generate-button')
+// When the button is clicked, render items
+let generate_button = document.querySelector('.generate-button');
 generate_button.addEventListener("click", renderItems);
-
-// renderItems();
-
 
 // Fetch gets your (local) JSON file…
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
@@ -72,59 +78,52 @@ fetch('csvjson.json')
 	.then(response => response.json())
 	.then(data => {
 		// And passes the data to the function, above!
-		(globaldata = data)
+		globaldata = data;
 	});
 
-
-// async function getData(){
-//     data.forEach(dataPoint) => {
-//         let time = dataPoint.time;
-//         let timevalue = document.getElementById("time").value;
-//         let effort = dataPoint.effort;
-//         let activity = dataPoint.activity;
-
-//         if (time.contains("5 min")){
-//             console.log("5 min");
-//         }
-//     }
-// };
-
-
+// Scroll to result section when button is clicked
 document.querySelector(".generate-button").addEventListener("click", () => {
 	document.getElementById("step6").scrollIntoView({ behavior: "smooth" });
-  });
+});
 
-
-  // i used https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
-  //https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
-// this was helpful for my work beccase it achieves the task i wanted 
-// query selector gets the generater button and the event its listening to is to guide the viewer to step 6 (the activity result page)
-
+// i used https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+// this was helpful for my work because it achieves the task i wanted 
+// query selector gets the generate button and the event it's listening to is to guide the viewer to step 6 (the activity result page)
 
 const sections = document.querySelectorAll("section[id^='step']");
 const navLinks = document.querySelectorAll(".nav-link");
 
+// these grab the elements that are section with step included. the part that says step 1 is a css selector and 
+// [id^='step'] means that it selects all elements with an id that starts with step
+// line 2 finds all .nav-link elements so when I have css with this it will grab them
 
-//these grab the elements that are section with step included. the part that says step 1 is a css selector and 
-//[id^='step'] means that it  selects all elements with an id that starts with step
-// line 2 finds alll .nav-link elements so when i have css with this it will grab them
-
-//continue writing hode HERE!!!!!!!!!
-
-
-  
 // this is the function that will highlight the nav link when the user scrolls to a section
-// it works by getting the  position of the scroll and relating it to the number / position of the sections to highligh ti 
+// it works by getting the position of the scroll and relating it to the number / position of the sections to highlight
 
 // resources
-//resources that helped me with this JS//
-//https://javascript.info/onscroll//
-//this helped me with the scrolling function
-//It was helpful for my project to build the vertical scroll nav 
-// it works by getting the current position of the scroll and relating it to the number / position of the sections//
-//it works to highlight the current section the user is lookjng at//
+// https://javascript.info/onscroll/ — helped me with the scrolling function
+// It was helpful for my project to build the vertical scroll nav 
+// it works by getting the current position of the scroll and relating it to the number / position of the sections
+// https://www.freecodecamp.org/news/how-to-create-a-scrollspy-with-javascript/
+// https://stackoverflow.com/questions/43500315/highlighting-links-based-on-scroll-position
+// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
 
-//https://www.freecodecamp.org/news/how-to-create-a-scrollspy-with-javascript///
-// https://stackoverflow.com/questions/43500315/highlighting-links-based-on-scroll-position?.com //
-//https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API//
+window.addEventListener("scroll", () => {
+	let current = "";
 
+	sections.forEach((section) => {
+		const sectionTop = section.offsetTop;
+		const sectionHeight = section.offsetHeight;
+		if (window.scrollY >= sectionTop - sectionHeight / 2) {
+			current = section.getAttribute("id");
+		}
+	});
+
+	navLinks.forEach((link) => {
+		link.classList.remove("active");
+		if (link.getAttribute("href").substring(1) === current) {
+			link.classList.add("active");
+		}
+	});
+});
